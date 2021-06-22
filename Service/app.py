373931e5f32c3c122d4197c7be7636c7e3739130
Dushyant Singh <Dushyant.Singh.Contractor@PepsiCo.com>
2021-06-23 00:36:54 +0530

    Added the changes for Python App, returns an Image based on input query params 🐍

--- Service/app.py
+++ Service/app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask import send_file
+from flask import request
+from PIL import Image, ImageDraw, ImageFont
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/data')
+def data():
+        slicerValues = request.args.get('Values')
+        slicerNames = request.args.get('Filters')
+        unixTime = request.args.get('EPOCH')
+        username = request.args.get('UserName')
+        text = 'Slicers: ' + slicerNames + '\nValues: ' + slicerValues \
+            + '\nTime: ' + unixTime + '\nUser Name: ' + username
+        font = \
+           ImageFont.truetype('/usr/share/fonts/truetype/hack/Hack-Bold.ttf'
+                            , 28, encoding='unic')
+        (text_width, text_height) = font.getsize(text)
+        canvas = Image.new('RGB', (text_width + 10, text_height * 6),
+                            'orange')
+        draw = ImageDraw.Draw(canvas)
+        draw.text((5, 5), text, 'blue', font)
+        canvas.save('unicode-text.png', 'PNG')
+        return send_file('unicode-text.png', mimetype='image/png')
